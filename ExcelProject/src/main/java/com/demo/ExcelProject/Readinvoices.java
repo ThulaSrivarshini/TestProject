691459ecb47b16afb1e18ade93c01180b15fcb16
Acer <Acer@DESKTOP-3O03SKB>
2023-05-14 17:03:15 +0530

    changed commit

--- ExcelProject/src/main/java/com/demo/ExcelProject/Readinvoices.java
+++ ExcelProject/src/main/java/com/demo/ExcelProject/Readinvoices.java
@@ -1,61 +1,61 @@
 package com.demo.ExcelProject;
-import java.io.*;
 
-import java.sql.*;
-import java.util.*;
-import static java.lang.Integer.parseInt;
-public class Readinvoices{
-	public static void main(String[]args) {
-		String jdbcUrl="jdbc:mysql://localhost:3306/info";
-		String username="root";
-		String password="Svdkf@678";
-		String FilePath="C:/Users/Acer/Downloads/Book.xlsx";
-		int batchSize=20;
-		Connection connection=null;
-		try {
-			connection=DriverManager.getConnection(jdbcUrl, username, password);
-			connection.setAutoCommit(false);
-			String sql="insert into student(id,name,address,salary) values(?,?,?,?)";
-			PreparedStatement statement=connection.prepareStatement(sql);
-			BufferedReader lineReader=new BufferedReader(new FileReader(FilePath));
-			String linetext=null;
-			int count=0;
-			lineReader.readLine();
-			//while((linetext=lineReader.readLine())!=null) {
-			//	String[] data=linetext.split(",");
-//				try {
-//				String id=data[0];
-//				String name=data[1];
-//				String address=data[2];
-//				String salary=data[3];
-//				}
-//				catch(ArrayIndexOutOfBoundsException e) {
-//					System.out.println("exception catched");
-//					statement.setString(1,id);
-//					statement.setString(2, name);
-//					statement.setString(3, address);
-//					statement.setString(4, salary);
-//				}
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-				//statement.addBatch();
-				if(count%batchSize==0) {
-					statement.executeBatch();
-				}
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Readinvoices {
+
+	public static void main(String[] args) throws SQLException, IOException {
 	
-			//}
-			lineReader.close();
-			statement.executeBatch();
-			connection.commit();
-			connection.close();
-			System.out.println("Data has been inserted successfully");
-
+		//Database connection
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student","root","Svdkf@678");
+		Statement stmt=con.createStatement();
+		
+		//create a new table in the database 'places'
+		String sql="create table info (Student_ID int, Student_Name varchar(40),Father_name varchar(40),Mother_name varchar(30),Adderss varchar(25),Country varchar(20),State varchar(40),Pincode int,Mobile_no int),Blood_grp varchar(40))";
+		stmt.execute(sql);
+		
+		//Excel
+		FileInputStream fis=new FileInputStream("C:/Users/Acer/Downloads/Studentinfo.xlsx");
+		XSSFWorkbook workbook=new XSSFWorkbook(fis);
+		XSSFSheet sheet=workbook.getSheet("Sheet1");
+		
+		int rows=sheet.getLastRowNum();
+		
+		for(int r=1;r<=rows;r++)
+		{
+			XSSFRow row=sheet.getRow(r);
+			double student_ID=row.getCell(0).getNumericCellValue();
+			String student_name=row.getCell(1).getStringCellValue();
+			String fathername=row.getCell(2).getStringCellValue();
+			String mothername=row.getCell(3).getStringCellValue();
+			String address=row.getCell(4).getStringCellValue();
+			String country=row.getCell(5).getStringCellValue();
+			String state=row.getCell(6).getStringCellValue();
+			int pincode=(int) row.getCell(7).getNumericCellValue();
+			int mobile=(int)row.getCell(8).getNumericCellValue();
+			String bloodgrp=row.getCell(9).getStringCellValue();
 			
-			
-			
+			sql="insert into info values('"+student_ID+"', '"+student_name+"', '"+fathername+"', '"+mothername+"', '"+address+"', '"+country+"', '"+state+"', '"+pincode+"', '"+mobile+"', '"+bloodgrp+"')";
+			stmt.execute(sql);
+			stmt.execute("commit");
 		}
-		catch(Exception exception) {
-			exception.printStackTrace();
-			
-		}
+		
+		
+		workbook.close();
+		fis.close();
+		con.close();
+		
+		System.out.println("Done!!");
+		
 	}
+
 }
